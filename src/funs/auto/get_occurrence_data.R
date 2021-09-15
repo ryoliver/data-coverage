c7fb2dca217521360ed426c525047d5a28788e69
Ruth Oliver <ruth.oliver@yale.edu>
2021-09-15 17:08:42 -0600

    test as character

--- src/funs/auto/get_occurrence_data.R
+++ src/funs/auto/get_occurrence_data.R
@@ -4,12 +4,12 @@ get_occurrence_data <- function(file_path){
   
   pts_raw = data.table::rbindlist(lapply(files, data.table::fread),use.names = TRUE)
   colnames(pts_raw) <- tolower(colnames(pts_raw))
-  
+  pts_raw$eventdate <- as.character(pts_raw$eventdate)
   if(file_path == wi_file_path){
     library(geohashTools)
     
     pts_raw$geohash <- gh_encode(pts_raw$latitude,pts_raw$longitude,precision = 5)
-    pts_raw$eventdate <- as.Date(rep(NA,nrow(pts_raw)))
+    pts_raw$eventdate <- as.character(rep(NA,nrow(pts_raw)))
     
     pts_raw <- pts_raw %>%
       rename("scientificname" = sp_binomial,
