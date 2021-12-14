ff47a31c43ab880e56416134b9883141795395c1
Ruth Oliver <ruth.oliver@yale.edu>
2021-12-14 14:28:40 -0800

    run wi only for birds

--- src/workflow/workflow.sh
+++ src/workflow/workflow.sh
@@ -23,7 +23,9 @@ fi
 #find covearge
 #sbatch $src/workflow/run_coverage.sh mammals 1950 2019 gbif-wi
 #sbatch $src/workflow/run_coverage.sh mammals 1950 2019 gbif
-sbatch $src/workflow/run_coverage.sh mammals 1950 2019 wi
+#sbatch $src/workflow/run_coverage.sh mammals 1950 2019 wi
+sbatch $src/workflow/run_coverage.sh birds 1950 2019 wi
+
 
 
 #sbatch $src/workflow/run_coverage_large.sh birds 1950 2019 gbif-wi
