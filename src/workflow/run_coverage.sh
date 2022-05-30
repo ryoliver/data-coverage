3cb4a27701ebcd33f0fca00f639d0312818522f2
Ruth Oliver <ruth.oliver@yale.edu>
2022-05-30 15:37:35 -0700

    remove bigmem

--- src/workflow/run_coverage.sh
+++ src/workflow/run_coverage.sh
@@ -6,7 +6,7 @@
 #SBATCH --mail-user=ruth.oliver@yale.edu
 #SBATCH --ntasks=1
 #SBATCH --mem-per-cpu=100g -t 2-
-#SBATCH --partition=general,bigmem,pi_jetz
+#SBATCH --partition=general,pi_jetz
 #SBATCH -C avx2
 
 module load R
