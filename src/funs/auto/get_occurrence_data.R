e82fadaed7d890dcb83661435474ba6168938f3c
Ruth Oliver <ruth.oliver@yale.edu>
2022-07-21 15:42:06 -0700

    update for new data, remove eventdate

--- src/funs/auto/get_occurrence_data.R
+++ src/funs/auto/get_occurrence_data.R
@@ -1,25 +1,41 @@
 get_occurrence_data <- function(file_path){
-  setwd(file_path)
-  files <- list.files(file_path,pattern = "*.csv",full.names = FALSE)
   
-  message("reading in files...")
-  pts_raw = data.table::rbindlist(lapply(files, data.table::fread),use.names = TRUE)
+  if (file_path == gbif_file_path && .data_source == "202207") {
+    file_path <- paste0(gbif_file_path,"gbif_",.taxa_name,"/csv")
+  }
+  
+  if (file_path != wi_file_path) {
+    setwd(file_path)
+    files <- list.files(file_path,pattern = "*.csv",full.names = FALSE)
+    
+    message("reading in files...")
+    pts_raw = data.table::rbindlist(lapply(files, data.table::fread),use.names = TRUE)
+  }
+  
+  if (file_path == wi_file_path) {
+    message("reading in files...")
+    pts_raw = fread(paste0(wi_file_path,"WI_data_coverage.csv"))
+    }
   
   message("check column names...")
   colnames(pts_raw) <- tolower(colnames(pts_raw))
   
-  message("check data format...")
-  pts_raw$eventdate <- as.character(pts_raw$eventdate)
+  #message("check data format...")
+  #pts_raw$eventdate <- as.character(pts_raw$eventdate)
   
   if(file_path == wi_file_path){
     library(geohashTools)
     
     pts_raw$geohash <- gh_encode(pts_raw$latitude,pts_raw$longitude,precision = 5)
-    pts_raw$eventdate <- as.character(rep(NA,nrow(pts_raw)))
+    #pts_raw$eventdate <- as.character(rep(NA,nrow(pts_raw)))
     
     pts_raw <- pts_raw %>%
-      rename("scientificname" = sp_binomial) %>%
-      select(scientificname,latitude,longitude,eventdate,year,geohash)
+      rename("scientificname" = sp_binomial) 
+      #select(scientificname,latitude,longitude,eventdate,year,geohash)
   }
+  
+  pts_raw <- pts_raw %>%
+    select(scientificname, year, geohash)
+  
   return(pts_raw)
 }
