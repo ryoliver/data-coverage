63b809976d9a27376ce4e5e1ae6cb018e5cca511
Ruth Oliver <ruth.oliver@yale.edu>
2022-07-26 12:44:22 -0700

    change R version

--- src/workflow/run_coverage_large.sh
+++ src/workflow/run_coverage_large.sh
@@ -9,6 +9,7 @@
 #SBATCH --partition=general,bigmem,pi_jetz
 #SBATCH -C avx2
 
-module load R/3.6.1-foss-2018b
+#module load R/3.6.1-foss-2018b
+module load R/4.1.0-foss-2020b
 
 Rscript /gpfs/ysm/project/jetz/ryo3/projects/data-coverage/src/poc/find_coverage.R $1 $2 $3 $4
